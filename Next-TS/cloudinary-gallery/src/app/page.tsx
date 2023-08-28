f628a6e8bf5651c51b4261d9216778bfe242fdd3
ahmadaliasghar <ahmadali2882@yahoo.com>
2023-08-28 21:00:16 +0500

    Starting my-gallery

---
+++ Next-TS/cloudinary-gallery/src/app/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { CldUploadButton } from 'next-cloudinary';
+import { CldImage } from 'next-cloudinary';
+import { useState } from 'react';
+
+export default function Home() {
+  const [imageId, setImageId] = useState("");
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <CldUploadButton onUpload={(result: any) => {
+          setImageId(result.info.public_id)
+        }} uploadPreset="bn1nm4qr" />
+        {imageId && <CldImage
+          width="960"
+          height="600"
+          src={imageId}
+          sizes="100vw"
+          alt="Description of my image"
+        />}
+        
+    </main>
+  )
+}
