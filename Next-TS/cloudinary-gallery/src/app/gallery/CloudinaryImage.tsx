688578cac94d4173b60de32659c79de8fcb4d890
ahmadaliasghar <ahmadali2882@yahoo.com>
2023-09-23 23:39:53 +0500

    Make Some Fixes in Gallery

--- Next-TS/cloudinary-gallery/src/app/gallery/CloudinaryImage.tsx
+++ Next-TS/cloudinary-gallery/src/app/gallery/CloudinaryImage.tsx
@@ -1,28 +1,25 @@
 'use client'
 import Heart from '@/assets/icons/Heart'
 import { CldImage } from 'next-cloudinary'
-import { useRouter } from 'next/navigation'
+import React, { useTransition } from 'react'
 import cloudinary from "cloudinary"
-import React from 'react'
 import { setFavouriteAction } from './action'
 
-const CloudinaryImage = (props:any) => {
-  
+const CloudinaryImage = (props:any & {publicId:string}) => {
+  const [transition, startTransition] = useTransition(); 
   return (
-    <div 
-    className='relative'>
+    <div className='relative'>
        <CldImage 
-        // width="500"
-        // height="300"
-        // src={public_id}
-        // sizes="100vw"
-        // alt="a phot of something"
         {...props}
         />
-        <Heart
+        <Heart 
+        className='absolute top-2 right-2 hover:text-red cursor-pointer'
         onClick={() => {
-          setFavouriteAction(props.publicId)
-        }} className='absolute top-2 right-2 hover:text-pink-400'/>
+          startTransition(()=> {
+            setFavouriteAction(props.publicId)
+          });
+        }}
+        />
     </div>
   )
 }
--- Next-TS/cloudinary-gallery/src/app/gallery/action.ts
+++ Next-TS/cloudinary-gallery/src/app/gallery/action.ts
@@ -1,7 +1,8 @@
+"use server"
 import cloudinary from "cloudinary";
 import { revalidatePath } from "next/cache";
 
 export async function setFavouriteAction(publicId: string) {
-  await cloudinary.v2.uploader.add_tag("favourite", [publicId]);
+  await cloudinary.v2.uploader.add_tag('favourite', [publicId])
   revalidatePath('/gallery');
 }
